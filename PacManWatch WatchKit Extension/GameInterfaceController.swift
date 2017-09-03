da18f261938a93c0d811cafbbf8d86927f365907
Wilsonator5000 <wilsonator5000@gmail.com>
2017-09-03 07:20:05 -0400

    Working title screen and character

---
+++ PacManWatch WatchKit Extension/GameInterfaceController.swift
@@ -0,0 +1,48 @@
+//
+//  GameInterfaceController.swift
+//  PacManWatch WatchKit Extension
+//
+//  Created by Wilson Gramer on 9/2/17.
+//  Copyright © 2017 Neef.co. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class GameInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var pacmanSequencePicker: WKInterfacePicker!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        let pickerItems: [WKPickerItem] = (0...10).map {
+            let pickerItem = WKPickerItem()
+            pickerItem.contentImage = WKImage(imageName: "pacman-pos-\(($0) - 1)")
+            return pickerItem
+        }
+        pacmanSequencePicker.setItems(pickerItems)
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    @IBAction func pickerSelectedItemChanged(value: Int) {
+        if value == 10 {
+            pacmanSequencePicker.setSelectedItemIndex(1)
+        }
+        if value == 0 {
+            pacmanSequencePicker.setSelectedItemIndex(9)
+        }
+    }
+}
+
--- PacManWatch WatchKit Extension/TitleInterfaceController.swift
+++ PacManWatch WatchKit Extension/TitleInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  TitleInterfaceController.swift
 //  PacManWatch WatchKit Extension
 //
 //  Created by Wilson Gramer on 9/2/17.
@@ -9,9 +9,12 @@
 import WatchKit
 import Foundation
 
+class TitleInterfaceController: WKInterfaceController {
 
-class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var titleGifImageView: WKInterfaceImage!
+    
+    var timer = Timer()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +24,21 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        titleGifImageView.setImageNamed("titlegif-frame-")
+        titleGifImageView.startAnimatingWithImages(in: NSMakeRange(0, 19), duration: 1, repeatCount: -1)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.playHapticOnTitleGif), userInfo: nil, repeats: true)
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        timer.invalidate()
+    }
+    
+    @objc func playHapticOnTitleGif() {
+        WKInterfaceDevice.current().play(.click)
     }
 
 }
